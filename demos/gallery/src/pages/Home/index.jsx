ec51405604573710dfd5b22c480a675bae8b5c0b
iAladdin <ialaddin@me.com>
2022-07-29 18:48:37 +0800

    upgrade kraken to v0.11.0

--- demos/gallery/src/pages/Home/index.jsx
+++ demos/gallery/src/pages/Home/index.jsx
@@ -14,21 +14,21 @@ export default function Home() {
       }}
       horizontal={true}
     >
-      <Card bundle="demos/hello-vue/dist/js/app.js" banner="../../../../demos/hello-vue/banner.png" title="Vue" />
-      <Card bundle="demos/hello-react/build/static/js/bundle.js" banner="../../../../demos/hello-react/banner.png" title="React" />
-      <Card bundle="demos/hello-rax/build/kraken/index.js" banner="../../../../demos/hello-rax/banner.png" title="Rax" />
+      <Card bundle="assets://assets/demos/hello-vue/dist/js/app.js" banner="../../../../demos/hello-vue/banner.png" title="Vue" />
+      <Card bundle="assets://assets/demos/hello-react/build/static/js/bundle.js" banner="../../../../demos/hello-react/banner.png" title="React" />
+      <Card bundle="assets://assets/demos/hello-rax/build/kraken/index.js" banner="../../../../demos/hello-rax/banner.png" title="Rax" />
     </ScrollView>
     <p className="title"><img className="icon" src="../../../../demos/gallery/icon/demo.png" />  Demo</p>
     <View className="home">
-      <Card bundle="demos/anime/build/kraken/index.js" banner="../../../../demos/anime/banner.png" title="Anime" desc="A animation demo built with anime.js" />
-      <Card bundle="demos/calendar/build/kraken/index.js" banner="../../../../demos/calendar/banner.png" title="Calendar" desc="Calendar component with kraken" />
-      <Card bundle="demos/granule/build/kraken/index.js" banner="../../../../demos/granule/banner.png" title="Granule" desc="Granule effect built with CSS transform" />
-      <Card bundle="demos/dragable-list/build/kraken/index.js" banner="../../../../demos/dragable-list/banner.png" title="Dragable List" desc="A re-orderable list that animates nicely built on top of react-motion" />
-      <Card bundle="demos/riddles/build/kraken/index.js" banner="../../../../demos/riddles/banner.png" title="Riddles" desc="A demo use nested and stylized text" />
-      <Card bundle="demos/data-grid/dist/js/app.js" banner="../../../../demos/data-grid/banner.png" title="Grid" desc="A data grid demo use Vue.js" />
-      <Card bundle="demos/todomvc/build/kraken/index.js" banner="../../../../demos/todomvc/banner.png" title="TodoMVC" desc="Todo application with kraken" />
-      <Card bundle="demos/wallet/build/kraken/index.js" banner="../../../../demos/wallet/banner.png" title="Wallet" desc="Wallet application with kraken" />
-      <Card bundle="demos/order/build/kraken/index.js" banner="../../../../demos/order/banner.png" title="Order" desc="Order page with kraken" />
+      <Card bundle="assets://assets/demos/anime/build/kraken/index.js" banner="../../../../demos/anime/banner.png" title="Anime" desc="A animation demo built with anime.js" />
+      <Card bundle="assets://assets/demos/calendar/build/kraken/index.js" banner="../../../../demos/calendar/banner.png" title="Calendar" desc="Calendar component with kraken" />
+      <Card bundle="assets://assets/demos/granule/build/kraken/index.js" banner="../../../../demos/granule/banner.png" title="Granule" desc="Granule effect built with CSS transform" />
+      <Card bundle="assets://assets/demos/dragable-list/build/kraken/index.js" banner="../../../../demos/dragable-list/banner.png" title="Dragable List" desc="A re-orderable list that animates nicely built on top of react-motion" />
+      <Card bundle="assets://assets/demos/riddles/build/kraken/index.js" banner="../../../../demos/riddles/banner.png" title="Riddles" desc="A demo use nested and stylized text" />
+      <Card bundle="assets://assets/demos/data-grid/dist/js/app.js" banner="../../../../demos/data-grid/banner.png" title="Grid" desc="A data grid demo use Vue.js" />
+      <Card bundle="assets://assets/demos/todomvc/build/kraken/index.js" banner="../../../../demos/todomvc/banner.png" title="TodoMVC" desc="Todo application with kraken" />
+      <Card bundle="assets://assets/demos/wallet/build/kraken/index.js" banner="../../../../demos/wallet/banner.png" title="Wallet" desc="Wallet application with kraken" />
+      <Card bundle="assets://assets/demos/order/build/kraken/index.js" banner="../../../../demos/order/banner.png" title="Order" desc="Order page with kraken" />
     </View>
   </>
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 }
 
 class GalleryApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -23,19 +22,21 @@ class MainScreen extends StatelessWidget {
     final MediaQueryData queryData = MediaQuery.of(context);
     final Size viewportSize = queryData.size;
     KrakenNavigationDelegate navigationDelegate = KrakenNavigationDelegate();
-    navigationDelegate.setDecisionHandler((KrakenNavigationAction action) async {
-      Navigator.push(context, MaterialPageRoute(builder: (_) {
-        return DetailScreen(action);
-      }));
+    // navigationDelegate
+    //     .setDecisionHandler((KrakenNavigationAction action) async {
+    //   Navigator.push(context, MaterialPageRoute(builder: (_) {
+    //     return DetailScreen(action);
+    //   }));
 
-      return KrakenNavigationActionPolicy.allow;
-    });
+    //   return KrakenNavigationActionPolicy.allow;
+    // });
 
     return SafeArea(
       child: Kraken(
         viewportWidth: viewportSize.width - queryData.viewPadding.horizontal,
         viewportHeight: viewportSize.height - queryData.viewPadding.vertical,
-        bundlePath: 'demos/gallery/build/kraken/index.js',
+        bundle: KrakenBundle.fromUrl(
+            'assets://assets/demos/gallery/build/kraken/index.js'),
         navigationDelegate: navigationDelegate,
       ),
     );
@@ -49,11 +50,11 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     // Fullscreen kraken view.
     return Scaffold(
-      body: Kraken(
-        viewportWidth: window.physicalSize.width / window.devicePixelRatio,
-        viewportHeight: window.physicalSize.height / window.devicePixelRatio,
-        bundlePath: action.target,
-      ),
+      // body: Kraken(
+      //   viewportWidth: window.physicalSize.width / window.devicePixelRatio,
+      //   viewportHeight: window.physicalSize.height / window.devicePixelRatio,
+      //   bundle: KrakenBundle.fromUrl(action.target),
+      // ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           Navigator.pop(context);
@@ -64,6 +65,5 @@ class DetailScreen extends StatelessWidget {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
     );
-
   }
 }

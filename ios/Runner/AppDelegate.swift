bdf7c9bad9256a687650111bb4d8a2fc580a3308
iAladdin <ialaddin@me.com>
2022-08-01 17:27:03 +0800

    enable iOS pro motion support

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,16 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if #available(iOS 15.0, *) {
+      let displayLink = CADisplayLink(target: self, selector: #selector(step))
+      displayLink.preferredFrameRateRange = CAFrameRateRange(minimum:80, maximum:120, preferred:120)
+      displayLink.add(to: .current, forMode: .default)
+      
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  @objc func step(displaylink: CADisplayLink) {
+    // Will be called once a frame has been built while matching desired frame rate
   }
 }
